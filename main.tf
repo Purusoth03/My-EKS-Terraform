60ff14632fe7e59d18245bfdd60403d79bed6a7d
Purusoth03 <purusoth03@protonmail.com>
2025-07-03 09:44:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "MY_EKS_vpc" {
+resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "MY_EKS-vpc"
+    Name = "eks-vpc"
   }
 }
 
-resource "aws_subnet" "MY_EKS_subnet" {
+resource "aws_subnet" "eks_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.MY_EKS_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.MY_EKS_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "MY_EKS-subnet-${count.index}"
+    Name = "eks-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "MY_EKS_igw" {
-  vpc_id = aws_vpc.MY_EKS_vpc.id
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   tags = {
-    Name = "MY_EKS-igw"
+    Name = "eks-igw"
   }
 }
 
-resource "aws_route_table" "MY_EKS_route_table" {
-  vpc_id = aws_vpc.MY_EKS_vpc.id
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.MY_EKS_igw.id
+    gateway_id = aws_internet_gateway.eks_igw.id
   }
 
   tags = {
-    Name = "MY_EKS-route-table"
+    Name = "eks-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.MY_EKS[count.index].id
-  route_table_id = aws_route_table.MY_EKS_route_table.id
+  subnet_id      = aws_subnet.eks_subnet[count.index].id
+  route_table_id = aws_route_table.eks_route_table.id
 }
 
-resource "aws_security_group" "MY_EKS_sg" {
-  vpc_id = aws_vpc.MY_EKS_vpc.id
+resource "aws_security_group" "eks_cluster_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "MY_EKS_sg" {
   }
 
   tags = {
-    Name = "MY_EKS-sg"
+    Name = "eks-cluster-sg"
   }
 }
 
-resource "aws_security_group" "MY_EKS_node_sg" {
-  vpc_id = aws_vpc.MY_EKS_vpc.id
+resource "aws_security_group" "eks_node_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "MY_EKS_node_sg" {
   }
 
   tags = {
-    Name = "MY_EKS-node-sg"
+    Name = "eks-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "MY_EKS" {
-  name     = "MY_EKS"
-  role_arn = aws_iam_role.MY_EKS_cluster_role.arn
+resource "aws_eks_cluster" "eks" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.MY_EKS_subnet[*].id
-    security_group_ids = [aws_security_group.MY_EKS_sg.id]
+    subnet_ids         = aws_subnet.eks_subnet[*].id
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "MY_EKS" {
-  cluster_name    = aws_eks_cluster.MY_EKS.name
-  node_group_name = "MY_EKS-node-group"
-  node_role_arn   = aws_iam_role.MY_EKS_node_group_role.arn
-  subnet_ids      = aws_subnet.MY_EKS_subnet[*].id
+resource "aws_eks_node_group" "eks" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = aws_subnet.eks_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -112,12 +112,12 @@ resource "aws_eks_node_group" "MY_EKS" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.MY_EKS_node_sg.id]
+    source_security_group_ids = [aws_security_group.eks_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "MY_EKS_cluster_role" {
-  name = "MY_EKS-role"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "MY_EKS_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "MY_EKS_cluster_role_policy" {
-  role       = aws_iam_role.MY_EKS_cluster_role.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "MY_EKS_node_group_role" {
-  name = "MY_EKS-node-group-role"
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,17 @@ resource "aws_iam_role" "MY_EKS_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "MY_EKS_node_group_role_policy" {
-  role       = aws_iam_role.MY_EKS_node_group_role.name
+resource "aws_iam_role_policy_attachment" "eks_node_group_role_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "MY_EKS_node_group_cni_policy" {
-  role       = aws_iam_role.MY_EKS_node_group_role.name
+resource "aws_iam_role_policy_attachment" "eks_node_group_cni_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "MY_EKS_node_group_registry_policy" {
-  role       = aws_iam_role.MY_EKS_node_group_role.name
+resource "aws_iam_role_policy_attachment" "eks_node_group_registry_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
